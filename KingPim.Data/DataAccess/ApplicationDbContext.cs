965f943e4bd4dd42de3890ce49d7b47c7a0604b1
malloryfraiche <malloryfraiche@gmail.com>
2018-10-17 14:12:43 +0200

    Added class in DataAccess folder. Deleted ExtentionMethods project and added folders in Infrastructure project with ExtentionMethods and Helpers.

---
+++ KingPim.Data/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using KingPim.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingPim.Data.DataAccess
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Subcategory> Subcategory { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<AttributeGroup> AttributeGroup { get; set; }
+        public DbSet<ProductAttribute> ProductAttribute { get; set; }
+        public DbSet<ProductAttributeValue> ProductAttributeValue { get; set; }
+    }
+}
