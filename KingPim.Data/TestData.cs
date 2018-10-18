67ebc8c1b990486acaab73b7469733a908d11b6a
malloryfraiche <malloryfraiche@gmail.com>
2018-10-18 14:16:31 +0200

    Added seed data for one category and for 3 different ProductAttributeValue connections.

---
+++ KingPim.Data/TestData.cs
@@ -0,0 +1,194 @@
+﻿using KingPim.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingPim.Data
+{
+    public class TestData
+    {
+        public static List<Category> GetTestData()
+        {
+            var productAttributeListOne = new List<ProductAttribute>();
+            var productAttributeDataOne = new ProductAttribute
+            {
+                Id = 1,
+                Name = "Röd",
+                Description = "Färg röd.",
+                Type = "string",
+                AttributeGroupId = 1
+            };
+            var productAttributeDataTwo = new ProductAttribute
+            {
+                Id = 2,
+                Name = "Blå",
+                Description = "Färg blå.",
+                Type = "string",
+                AttributeGroupId = 1
+            };
+            productAttributeListOne.Add(productAttributeDataOne);
+            productAttributeListOne.Add(productAttributeDataTwo);
+
+            var productAttributeListTwo = new List<ProductAttribute>();
+            var productAttributeDataThree = new ProductAttribute
+            {
+                Id = 3,
+                Name = "Batteri",
+                Description = "Behöver produkt batterier?",
+                Type = "bool",
+                AttributeGroupId = 2
+            };
+            productAttributeListTwo.Add(productAttributeDataThree);
+
+            var attributeGroupListOne = new List<AttributeGroup>();
+            var attributeGroupDataOne = new AttributeGroup
+            {
+                Id = 1,
+                Name = "Färg",
+                Description = "Färg av produkt.",
+                ProductAttributes = productAttributeListOne
+            };
+            var attributeGroupDataTwo = new AttributeGroup
+            {
+                Id = 2,
+                Name = "Teknisk",
+                Description = "Alla information om produktens teknisk behövs.",
+                ProductAttributes = productAttributeListTwo
+            };
+            attributeGroupListOne.Add(attributeGroupDataOne);
+            attributeGroupListOne.Add(attributeGroupDataTwo);
+            
+            var productListOne = new List<Product>();
+            var productDataOne = new Product
+            {
+                Id = 1,
+                Name = "Titlest ProV1 Balls",
+                Description = "Ett dussin högeffektiv bollar som många proffs använda.",
+                Price = 350,
+                SubcategoryId = 1,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            var productDataTwo = new Product
+            {
+                Id = 2,
+                Name = "Ping Bag",
+                Description = "Modernt och lättvikt golfbag optimal för att har i alla typ av väder.",
+                Price = 750,
+                SubcategoryId = 1,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            productListOne.Add(productDataOne);
+            productListOne.Add(productDataTwo);
+            
+            var productListTwo = new List<Product>();
+            var productDataThree = new Product
+            {
+                Id = 3,
+                Name = "ECCO Shoes",
+                Description = "Vattentät och bekväm skor som man kan gå i flera timma utan skosår.",
+                Price = 1200,
+                SubcategoryId = 2,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            var productDataFour = new Product
+            {
+                Id = 4,
+                Name = "Nike Polo Shirt",
+                Description = "Passformen är precis lagom, inte för tight och inte för löst sittande.",
+                Price = 395,
+                SubcategoryId = 2,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            productListTwo.Add(productDataThree);
+            productListTwo.Add(productDataFour);
+            
+            var subcatListOne = new List<Subcategory>();
+            var subcategoryDataOne = new Subcategory
+            {
+                Id = 1,
+                Name = "Utrustning",
+                CategoryId = 1,
+                Products = productListOne,
+                AttributeGroups = attributeGroupListOne,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            var subcategoryDataTwo = new Subcategory
+            {
+                Id = 2,
+                Name = "Kläder",
+                CategoryId = 1,
+                Products = productListTwo,
+                AttributeGroups = attributeGroupListOne,
+                UpdatedDate = DateTime.Now.Date,
+                AddedDate = DateTime.Today,
+                Published = false,
+                Version = 1.0
+            };
+            subcatListOne.Add(subcategoryDataOne);
+            subcatListOne.Add(subcategoryDataTwo);
+
+            var categoryList = new List<Category>
+            {
+                new Category
+                {
+                    Id = 1,
+                    Name = "Golf",
+                    Subcategories = subcatListOne,
+                    UpdatedDate = DateTime.Now.Date,
+                    AddedDate = DateTime.Today,
+                    Published = false,
+                    Version = 1.0
+                }
+            };
+
+            return categoryList;
+        }
+
+        public static List<ProductAttributeValue> GetProductAttributeValues()
+        {
+            var productAttributeValueList = new List<ProductAttributeValue>();
+            var prodAttributeValOne = new ProductAttributeValue
+            {
+                Id = 1,
+                Value = "Röd",
+                ProductAttributeId = 1,
+                ProductId = 2
+            };
+            var prodAttributeValTwo = new ProductAttributeValue
+            {
+                Id = 2,
+                Value = "Röd",
+                ProductAttributeId = 1,
+                ProductId = 3
+            };
+            var prodAttributeValThree = new ProductAttributeValue
+            {
+                Id = 3,
+                Value = "true",
+                ProductAttributeId = 3,
+                ProductId = 1
+            };
+            productAttributeValueList.Add(prodAttributeValOne);
+            productAttributeValueList.Add(prodAttributeValTwo);
+            productAttributeValueList.Add(prodAttributeValThree);
+
+            return productAttributeValueList;
+        }
+
+    }
+}
--- KingPim.Web/Controllers/TestController.cs
+++ KingPim.Web/Controllers/TestController.cs
@@ -2,55 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KingPim.Data;
 using KingPim.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KingPim.Web.Controllers
 {
-    [Route("api/[controller]")]
     //[ApiController]
     public class TestController : Controller
     {
-        private List<Category> GetTestData()
+        [Route("api/[controller]/[action]")]
+        public IActionResult GetJsonCatTestData()
         {
-            var subcatList = new List<Subcategory>();
-            var subcategoryDataOne = new Subcategory
-            {
-                Id = 1,
-                Name = "Equiptment"
-            };
-            subcatList.Add(subcategoryDataOne);
-
-            var categoryList = new List<Category>
-            {
-                new Category
-                {
-                    Id = 1,
-                    Name = "Basket",
-                    Subcategories = subcatList
-                },
-                new Category
-                {
-                    Id = 2,
-                    Name = "Alpint",
-                    Subcategories = subcatList
-                },
-                new Category
-                {
-                    Id = 3,
-                    Name = "Crossfit",
-                    Subcategories = subcatList
-                }
-            };
-
-            return categoryList;
+            var catData = TestData.GetTestData();
+            return Json(catData);
         }
 
-        public IActionResult GetJsonTestData()
+        [Route("api/[controller]/[action]")]
+        public IActionResult GetJsonProductAttrValTestData()
         {
-            var data = GetTestData();
-            return Json(data);
+            var productAttrValData = TestData.GetProductAttributeValues();
+            return Json(productAttrValData);
         }
     }
 }
