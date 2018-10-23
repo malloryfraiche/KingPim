89fee2faf290495023287a1cad27a928683fe008
malloryfraiche <malloryfraiche@gmail.com>
2018-10-23 11:26:16 +0200

    Added AddNewVC view component and Components folder in project.

---
+++ KingPim.Web/Components/AddNewVC.cs
@@ -0,0 +1,24 @@
+﻿using KingPim.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KingPim.Web.Components
+{
+    public class AddNewVC : ViewComponent
+    {
+        private ICategoryRepository _categoryRepo;
+        public AddNewVC(ICategoryRepository categoryRepo)
+        {
+            _categoryRepo = categoryRepo;
+        }
+
+        public string Invoke()
+        {
+            var test = "Hello from the AddNewVC!";
+            return test;
+        }
+    }
+}
