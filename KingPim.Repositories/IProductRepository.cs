20b1c15ff7b997c70fc852696437c9531f26c61b
malloryfraiche <malloryfraiche@gmail.com>
2018-10-30 16:15:33 +0100

    Created Product repo, interface, controller, and view. Built GetAllProduct function.

---
+++ KingPim.Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using KingPim.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingPim.Repositories
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        IEnumerable<Product> GetAllProducts();
+    }
+}
