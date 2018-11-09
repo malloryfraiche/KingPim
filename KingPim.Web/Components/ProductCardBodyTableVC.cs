b3d18192996bf1991a76207045bee63c78cecb90
malloryfraiche <malloryfraiche@gmail.com>
2018-11-09 14:24:11 +0100

    Fixed the products card body to show the correct information.

--- KingPim.Web/Components/ProductCardBodyTableVC.cs
+++ KingPim.Web/Components/ProductCardBodyTableVC.cs
@@ -53,27 +53,18 @@ namespace KingPim.Web.Components
             //}
 
 
+
+
+
             
             // To get the product (from the product id the card passed in).
             var theProduct = _productRepo.Products.FirstOrDefault(p => p.Id.Equals(productId));
             // To get the products subcategory.
             var productsSubcategoryId = theProduct.Subcategory.Id;
-            
-            // To have all info from the SubcategoryAttributeGroup DB.
-            var subcategoryAttributeGroups = _subcategoryAttributeGroupRepo.SubcategoryAttributeGroups;
-            var theCardsSubcategoryAttributeGroups = new List<AttributeGroup>();
-            // To get each subcatId so can get the subcategories different AttrGroups it has.
-            foreach (var subcatAttrGroup in subcategoryAttributeGroups)
-            {
-                // if the subcatId matches the products subcategory id, push the attrGroup to the list...
-                if (subcatAttrGroup.SubcategoryId == productsSubcategoryId)
-                {
-                    theCardsSubcategoryAttributeGroups.Add(subcatAttrGroup.AttributeGroup);
-                }
-            }
-            
+
             // To have all info from the ProductAttributeValue DB.
             var productAttributeValues = _productAttrValRepo.ProductAttributeValues;
+            // List to store the specific cards product attribute value data.
             var theCardsProductAttributeValues = new List<ProductAttributeValue>();
             foreach (var productAttrValueRow in productAttributeValues)
             {
@@ -85,32 +76,105 @@ namespace KingPim.Web.Components
 
 
 
-
-
-
-
-            if (theCardsSubcategoryAttributeGroups != null)
+            // To have all info from the SubcategoryAttributeGroup DB.
+            var subcategoryAttributeGroups = _subcategoryAttributeGroupRepo.SubcategoryAttributeGroups;
+            var theCardsSubcategoryAttributeGroups = new List<SubcategoryAttributeGroup>();
+            // To get each subcatId so can get the subcategories different AttrGroups it has.
+            foreach (var subcatAttrGroup in subcategoryAttributeGroups)
             {
-
-                var productAttributeValueVM = new ProductAttributeValueViewModel
+                // if the subcatId matches the products subcategory id, push the subcategory attribute group row to the list...
+                if (subcatAttrGroup.SubcategoryId== productsSubcategoryId)
                 {
-                    Id = productId,
-                    AttributeGroups = theCardsSubcategoryAttributeGroups,
-                    ProductAttributeValues = theCardsProductAttributeValues
-
-                };
-
-                return View(productAttributeValueVM);
-
+                    theCardsSubcategoryAttributeGroups.Add(subcatAttrGroup);
+                }
             }
+
+
+
+            // To send in filtered info for the specific product on the card.
+            // If the product has information in the ProductAttributeValue DB table...
+            if (theCardsProductAttributeValues.Any())
+            {
+                var prodAttrValVM = new ProductAttributeValueViewModel
+                {
+                    CardProductAttributeValues = theCardsProductAttributeValues,
+                    CardSubcategoryAttributeGroups = theCardsSubcategoryAttributeGroups
+                };
+                return View(prodAttrValVM);
+            }
+            // If not just show all the product attributes connected to the products subcategory (the 'value' from the ProductAttributeValue DB table will be empty)..
             else
             {
-                var productAttributeValueVM = new ProductAttributeValueViewModel
+                var prodAttrValVM = new ProductAttributeValueViewModel
                 {
-
+                    CardSubcategoryAttributeGroups = theCardsSubcategoryAttributeGroups
                 };
-                return View(productAttributeValueVM);
+                return View(prodAttrValVM);
             }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            //// To have all info from the SubcategoryAttributeGroup DB.
+            //var subcategoryAttributeGroups = _subcategoryAttributeGroupRepo.SubcategoryAttributeGroups;
+            //var theCardsSubcategoryAttributeGroups = new List<AttributeGroup>();
+            //// To get each subcatId so can get the subcategories different AttrGroups it has.
+            //foreach (var subcatAttrGroup in subcategoryAttributeGroups)
+            //{
+            //    // if the subcatId matches the products subcategory id, push the attrGroup to the list...
+            //    if (subcatAttrGroup.SubcategoryId == productsSubcategoryId)
+            //    {
+            //        theCardsSubcategoryAttributeGroups.Add(subcatAttrGroup.AttributeGroup);
+            //    }
+            //}
+
+            //// To have all info from the ProductAttributeValue DB.
+            //var productAttributeValues = _productAttrValRepo.ProductAttributeValues;
+            //var theCardsProductAttributeValues = new List<ProductAttributeValue>();
+            //foreach (var productAttrValueRow in productAttributeValues)
+            //{
+            //    if (productId == productAttrValueRow.ProductId)
+            //    {
+            //        theCardsProductAttributeValues.Add(productAttrValueRow);
+            //    }
+            //}
+
+
+            //if (theCardsSubcategoryAttributeGroups != null)
+            //{
+
+            //    var productAttributeValueVM = new ProductAttributeValueViewModel
+            //    {
+            //        Id = productId,
+            //        AttributeGroups = theCardsSubcategoryAttributeGroups,
+            //        ProductAttributeValues = theCardsProductAttributeValues
+
+            //    };
+
+            //    return View(productAttributeValueVM);
+
+            //}
+            //else
+            //{
+            //    var productAttributeValueVM = new ProductAttributeValueViewModel
+            //    {
+
+            //    };
+            //    return View(productAttributeValueVM);
+            //}
+
+
+
+
 
 
 
