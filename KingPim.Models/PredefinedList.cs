051501664cd315d4b0e08a87491aa0daeb7e0004
malloryfraiche <malloryfraiche@gmail.com>
2018-11-15 18:03:18 +0100

    Added PredefinedList and PredefinedListOption models. Updated ProductAttribute model to have an FK from PredefinedList.

---
+++ KingPim.Models/PredefinedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KingPim.Models
+{
+    public class PredefinedList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<PredefinedListOption> PredefinedListOptions { get; set; }
+    }
+}
--- KingPim.Models/PredefinedListOption.cs
+++ KingPim.Models/PredefinedListOption.cs
@@ -4,14 +4,10 @@ using System.Text;
 
 namespace KingPim.Models
 {
-    public class ProductAttribute
+    public class PredefinedListOption
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Type { get; set; }
-        public virtual AttributeGroup AttributeGroup { get; set; }
-        public int? AttributeGroupId { get; set; }
         public virtual PredefinedList PredefinedList { get; set; }
         public int? PredefinedListId { get; set; }
     }
